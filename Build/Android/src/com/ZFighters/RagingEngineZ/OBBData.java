dd5b17fec11376da8f96eb6e496fe7f5e9b2cd14
VorticalCookie <92548003+VorticalCookie@users.noreply.github.com>
2026-03-23 19:24:27 -0700

    Spring Break Build

--- Build/Android/src/com/ZFighters/RagingEngineZ/OBBData.java
+++ Build/Android/src/com/ZFighters/RagingEngineZ/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"2", // the version of the APK that the file was uploaded against
-49594355L // the length of the file in bytes
+"4", // the version of the APK that the file was uploaded against
+50578545L // the length of the file in bytes
 )
 };
 };
