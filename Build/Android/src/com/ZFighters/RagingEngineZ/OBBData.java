9bb83599e7fbee5101d007c19559ce9113681c0b
VorticalCookie <92548003+VorticalCookie@users.noreply.github.com>
2026-04-07 11:57:49 -0700

    AlphaBuild2.0

--- Build/Android/src/com/ZFighters/RagingEngineZ/OBBData.java
+++ Build/Android/src/com/ZFighters/RagingEngineZ/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"5", // the version of the APK that the file was uploaded against
-80564466L // the length of the file in bytes
+"6", // the version of the APK that the file was uploaded against
+85808453L // the length of the file in bytes
 )
 };
 };
